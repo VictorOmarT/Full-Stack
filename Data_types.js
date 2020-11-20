74953c933fbecffee8ae612c5c38d27ec3865481
VictorOmarT.M <33044106+VictorOmarT@users.noreply.github.com>
2020-11-19 18:58:41 -0600

    19/11

---
+++ Data_types.js
@@ -0,0 +1,11 @@
+console.log("JavaScript");
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49);
+// Strings 
+
+console.log(29 + 3.5)
+console.log(2020 - 1963)
+console.log("Hello" + " " + "World")
+console.log("Teaching the world how to code".length)
+console.log(Math.random() * 100)
