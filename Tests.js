269007081f50ae4526791bb8046ca579e97b28ec
VictorOmarT.M <33044106+VictorOmarT@users.noreply.github.com>
2020-12-10 18:59:57 -0600

    10/12

--- Tests.js
+++ Tests.js
@@ -82,6 +82,26 @@ function howOld(age,year){
   console.log(howOld(29,2019)) */
 
 
+  // Write your code below
+// Write your code below
+// Write your code below
+let bobsFollowers = ["kiki","cucu","cola","pipi"]
+let tinasFollowers = ["pipi","caca","cola"]
+let mutualFollowers = []
+
+for(let b = 0; b < bobsFollowers.length; b++){
+  for(let t = 0; t < tinasFollowers.length; t++ ){
+    if (bobsFollowers[b] === tinasFollowers[t]){
+      mutualFollowers.push(bobsFollowers[b])
+    }
+  }
+}
+
+console.log(mutualFollowers)
+
+
+
+
 
   
   
