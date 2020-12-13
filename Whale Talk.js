823ca5a676506658a550b04b607a2fcaf7098c67
VictorOmarT.M <33044106+VictorOmarT@users.noreply.github.com>
2020-12-12 19:07:57 -0600

    12/12

---
+++ Whale Talk.js
@@ -0,0 +1,10 @@
+let input = "Hola mundo"
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = []
+
+for (let i = 0; i < input.length; i ++){
+    for(let j=0; j<vowels.length; j ++){
+        //console.log(`${input[i]} ${vowels[j]}`)
+    }
+    //console.log(i)
+}
